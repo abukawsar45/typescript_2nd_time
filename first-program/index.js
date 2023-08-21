60af0c30500008b1cd3730ea7d5bb4b9cebb0f3a
abukawsar45 <kawsar1146236@gmail.com>
2023-08-21 15:56:32 +0600

    add function

---
+++ first-program/index.js
@@ -0,0 +1,10 @@
+var userName = 'AKS';
+console.log(userName);
+var userName2 = 'AKS2';
+console.log(userName2);
+// function
+var sum = function (num1, num2) {
+    var total = num1 + num2;
+    console.log(total);
+};
+sum(3, 34);
---
+++ first-program/index.ts
@@ -0,0 +1,12 @@
+let userName = 'AKS';
+console.log(userName)
+let userName2 = 'AKS2';
+console.log(userName2)
+
+// function
+const sum =(num1:number, num2: number)=>{
+  const total = num1+ num2;
+  console.log(total);
+}
+
+sum(3,34)
