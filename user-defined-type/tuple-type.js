8580c3c2e96c2abbaf1b0a076089985ddce36b39
abukawsar45 <kawsar1146236@gmail.com>
2023-08-22 00:20:52 +0600

    tuple declare

---
+++ user-defined-type/tuple-type.js
@@ -0,0 +1,4 @@
+// mixed data -key, value;
+var user;
+user = ['aks', true, 324];
+console.log(user[12]);
---
+++ user-defined-type/tuple-type.ts
@@ -0,0 +1,5 @@
+// mixed data -key, value;
+let user: (string | number | boolean)[];
+
+user = ['aks', true, 324]
+console.log(user[12])
