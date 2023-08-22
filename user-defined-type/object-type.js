20b53528c0df1a0936c90655aced4415b7c224c6
abukawsar45 <kawsar1146236@gmail.com>
2023-08-22 09:49:39 +0600

    object type with obtional chain

--- user-defined-type/object-type.js
+++ user-defined-type/object-type.js
@@ -1,4 +1,4 @@
 // let user1: { userName: string; age: number;}
 var user1;
-user1 = { userName: 'Abu Kowsear', age: 23, isStudent: false };
+user1 = { userName: 'Abu Kowsear', age: 23 };
 console.log(user1);
--- user-defined-type/object-type.ts
+++ user-defined-type/object-type.ts
@@ -1,6 +1,6 @@
 // let user1: { userName: string; age: number;}
 
-let user1: { userName: string, age: number, isStudent: boolean};
-user1 = { userName: 'Abu Kowsear', age: 23, isStudent: false};
+let user1: { userName: string, age: number, isStudent?: boolean};
+user1 = { userName: 'Abu Kowsear', age: 23};
 
 console.log(user1)
